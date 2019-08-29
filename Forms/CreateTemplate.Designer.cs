1f979e8c609a14e430bab71341292b5662490ea5
Mateusz Baranski <mateusz.baranski@ericsson.com>
2019-08-29 11:46:27 +0200

    better window

--- Forms/CreateTemplate.Designer.cs
+++ Forms/CreateTemplate.Designer.cs
@@ -57,6 +57,7 @@
             // 
             // button1
             // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Right;
             this.button1.Location = new System.Drawing.Point(687, 148);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(101, 56);
@@ -67,7 +68,8 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(687, 380);
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(687, 435);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(101, 58);
             this.button2.TabIndex = 4;
@@ -77,6 +79,7 @@
             // 
             // label2
             // 
+            this.label2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(41, 467);
             this.label2.Name = "label2";
@@ -86,6 +89,7 @@
             // 
             // textBox2
             // 
+            this.textBox2.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
             this.textBox2.Location = new System.Drawing.Point(177, 467);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(464, 26);
