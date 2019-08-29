df308e50cf968d0539c647fa0eca221e5e68d642
Kar0lLasek <19karolasek98@gmail.com>
2019-08-29 10:07:32 +0200

    Load button v1.0

--- Data/LoadData.cs
+++ Data/LoadData.cs
@@ -11,18 +11,38 @@ namespace FormPlugin.Data
 {
     public class LoadData
     {
-        Outlook.Application oApp = new Outlook.Application();
-
+        private string pathFile;
+        private string mailTo = "";
+        private string subject = "";
         public void createMail()
         {
             Outlook.Application oApp = new Outlook.Application();
             //Outlook._MailItem oMailItem = (Outlook._MailItem)oApp.CreateItem(Outlook.OlItemType.olMailItem);
             Outlook.MailItem mail =
-                oApp.CreateItemFromTemplate(
-                "C:\\Users\\ELASKAR\\AppData\\Roaming\\Microsoft\\Templates\\lol.oft") as Outlook.MailItem;
-            mail.To = "karower98@gmail.com";
-            // body, bcc etc...
+                oApp.CreateItemFromTemplate(pathFile) as Outlook.MailItem;
+            
+            //check if the mailto is fill
+            if(mailTo.Length > 0)
+                mail.To = mailTo;
+            //check if the subject is fill
+            if(subject.Length > 0)
+                mail.Subject = subject;
             mail.Display(true);
+        }
+
+        public void setPathFile(string path)
+        {
+            pathFile = path;
+        }
+
+        public void setMailTo(string to)
+        {
+            mailTo = to;
+        }
+
+        public void setSubject(string sub)
+        {
+            subject = sub;
         }
     }
 }
--- Forms/LoadTemplate.cs
+++ Forms/LoadTemplate.cs
@@ -1,37 +1,66 @@
 ﻿using FormPlugin.Data;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace FormPlugin.Forms
 {
     public partial class LoadTemplate : Form
     {
+        bool checkTemplate = false;
+        LoadData loadData;
         public LoadTemplate()
         {
             InitializeComponent();
+            loadData = new LoadData();
         }
 
         private void Button1_Click(object sender, EventArgs e)
         {
-            Close();
-            //Thread.Sleep(1000);
-            LoadData loadData = new LoadData();
-            loadData.createMail();
+            if (checkTemplate)
+            {
+                Close();
+                loadData.createMail();
+            }
+            else
+                MessageBox.Show("First choose your template", "Warning");
+            
         }
 
         private void Button2_Click(object sender, EventArgs e)
         {
             OpenFileDialog openFileDialog = new OpenFileDialog();
+            string pathTemplate = Environment.GetFolderPath(Environment.SpecialFolder.Templates);
+            openFileDialog.InitialDirectory = pathTemplate;
+            //openFileDialog.DefaultExt = "oft";
+            openFileDialog.Filter = "oft files (*.oft)|*.oft|All files (*.*)|*.*";
+            openFileDialog.RestoreDirectory = false;
+            openFileDialog.Title = "Template for email";
 
-            //if(openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult)
+
+            if (openFileDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string path = openFileDialog.FileName;
+                loadData.setPathFile(path);
+                checkTemplate = true;
+                label3.Text = openFileDialog.SafeFileName;
+            }
+        }
+
+        private void Label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+            string to = textBox1.Text;
+            loadData.setMailTo(to);
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+            string subject = textBox2.Text;
+            loadData.setSubject(subject);
         }
     }
 }
