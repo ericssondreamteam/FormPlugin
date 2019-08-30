0240bdb45381437fce3000203069784059453bbc
Mateusz Baranski <mateusz.baranski@ericsson.com>
2019-08-30 13:12:25 +0200

    lekkkie refakto i sprawdzaanie czy zawiera kazda linie z tempaolte

--- Data/CheckMail.cs
+++ Data/CheckMail.cs
@@ -1,11 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.Office.Interop.Outlook;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Exception = System.Exception;
-using System.Diagnostics;
 using System.Windows.Forms;
 using Application = Microsoft.Office.Interop.Outlook.Application;
 
@@ -13,7 +8,6 @@ namespace FormPlugin
 {
     public class CheckMail
     {
-        public const String question = "Podaj swój ulubiony kolor:";
         public MailItem mailItem;
         private string filePath;
 
@@ -28,14 +22,22 @@ namespace FormPlugin
             Application app = new Application();
             Folder folder = app.Session.GetDefaultFolder(defaultFolder) as Folder;
             MailItem mail = app.CreateItemFromTemplate(filePath, folder) as MailItem;
-            mail.Save();
+            List<string> templateLines, receviedMailLines;
+            String body = mailItem.Body;            
+            String templateBody = mail.Body;
+            receviedMailLines = getEmailLineByLine(body);
+            templateLines = getEmailLineByLine(templateBody);
+            bool conteinsAllTemplateLine = true;
+            foreach(string s in templateLines)
+            {
+                if(!body.Contains(s))
+                {
+                    conteinsAllTemplateLine = false;
+                    break;
+                }
+            }
 
-            String formBody = mail.Body;//this is template
-            funWithString(formBody);
-            String body = mailItem.Body;
-            bool myBool = body.Contains(formBody);
-
-            if (myBool == true)
+            if (conteinsAllTemplateLine == true)
             {
                 MessageBox.Show("OK");
             }
@@ -45,20 +47,16 @@ namespace FormPlugin
             }
         }
 
-        private void funWithString(string formBody)
-        {//"Hi,\r\nplease fill form ;)\r\n\r\n1. Ulubiony kolor\r\n\r\n\r\n2. Podaj swój ulubiony kolor:\r\n\r\n\r\n"
-            string[] cos = formBody.Replace("\r","").Split('\n');
-            List<string> list = new List<string>();
+        private List<string> getEmailLineByLine(string emailContent)
+        {
+            string[] cos = emailContent.Replace("\r","").Split('\n');
+            List<string> nextLineOfEmail = new List<string>();
             foreach (string s in cos)
             {
                 if (!s.Equals(""))
-                    list.Add(s);
-            }
-            foreach(string s in list)
-            {
-                Debug.WriteLine(s);
-            }
-
+                    nextLineOfEmail.Add(s);
+            }           
+            return nextLineOfEmail;
         }
 
         public void setFilePath(string path)
