1e46bb63bb207cea8f0249481eafde4568222557
Mateusz Baranski <mateusz.baranski@ericsson.com>
2019-09-02 08:59:20 +0200

    adding SUPER tip :O ;)

--- Forms/Check.Designer.cs
+++ Forms/Check.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
@@ -44,6 +45,7 @@
             // 
             // button2
             // 
+            this.button2.AccessibleDescription = "";
             this.button2.Location = new System.Drawing.Point(119, 282);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(134, 67);
@@ -52,17 +54,30 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.Button2_Click);
             // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 380);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(289, 40);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "If You created template called \"Default\"\n then You have less job";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
             // Check
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(367, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Check";
             this.Text = "Check";
             this.Load += new System.EventHandler(this.Check_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -70,5 +85,6 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
     }
 }
