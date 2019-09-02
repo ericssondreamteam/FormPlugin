01a380843dbbf12992f626b39cca7585c6b33281
Mateusz Baranski <mateusz.baranski@ericsson.com>
2019-09-02 08:39:46 +0200

    no jakos dziala, ale raczej mozna by jakos ladniej i wgl tak jakos brzydko :/

--- Data/Tools.cs
+++ Data/Tools.cs
@@ -13,7 +13,31 @@ namespace FormPlugin.Data
             bool haveWe=true;
             for(int i = 0; i < template.Count; i++)
             {
-                for(int j = 0; j < recieivedMail.Count; j++)
+                if (i == template.Count - 1)
+                {
+                    for (int l = 0; l < recieivedMail.Count; l++)
+                    {
+                        if (template[i].Equals(recieivedMail[l]) && FirstCharacterIsNumber(template[i]))
+                        {
+                            try
+                            {
+                                _ = recieivedMail[l + 1];
+                                return true;
+                            }
+                            catch (IndexOutOfRangeException e)
+                            {
+                                _ = e.Message;
+                                return false;
+                            }
+                            catch (ArgumentOutOfRangeException e)
+                            {
+                                _ = e.Message;
+                                return false;
+                            }
+                        }
+                    }
+                }
+                for (int j = 0; j < recieivedMail.Count; j++)
                 {
                     if(template[i].Equals(recieivedMail[j]) && FirstCharacterIsNumber(template[i]))
                     {
@@ -35,6 +59,7 @@ namespace FormPlugin.Data
                             _ = e.Message;
                         }
                     }
+                    
                 }
             }
             return haveWe;
