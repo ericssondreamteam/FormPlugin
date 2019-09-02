7a3689f6bdefe00c381839ae88cfe64bcbefeca8
Mateusz Baranski <mateusz.baranski@ericsson.com>
2019-09-02 12:49:31 +0200

    messageBox kiedy przychodzi nowy emial- mozemy robic co chcemy

--- ThisAddIn.cs
+++ ThisAddIn.cs
@@ -5,13 +5,42 @@ using System.Text;
 using System.Xml.Linq;
 using Outlook = Microsoft.Office.Interop.Outlook;
 using Office = Microsoft.Office.Core;
+using System.Windows.Forms;
 
 namespace FormPlugin
 {
     public partial class ThisAddIn
     {
+        Outlook.NameSpace outlookNameSpace;
+        Outlook.MAPIFolder inbox;
+        Outlook.Items items;
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
+            outlookNameSpace = this.Application.GetNamespace("MAPI");
+            inbox = outlookNameSpace.GetDefaultFolder(
+                    Microsoft.Office.Interop.Outlook.
+                    OlDefaultFolders.olFolderInbox);
+
+            items = inbox.Items;
+            items.ItemAdd +=
+                new Outlook.ItemsEvents_ItemAddEventHandler(items_ItemAdd);
+        }
+        void items_ItemAdd(object Item)
+        {
+            string filter = "USED CARS";
+            Outlook.MailItem mail = (Outlook.MailItem)Item;
+            if (Item != null)
+            {
+                MessageBox.Show(mail.Subject);
+               /* if (mail.MessageClass == "IPM.Note" &&
+                           mail.Subject.ToUpper().Contains(filter.ToUpper()))
+                {
+                    mail.Move(outlookNameSpace.GetDefaultFolder(
+                        Microsoft.Office.Interop.Outlook.
+                        OlDefaultFolders.olFolderJunk));
+                }*/
+            }
+
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
