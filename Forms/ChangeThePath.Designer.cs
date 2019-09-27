d94371e4cccdcb70101ee4e43029f47845188c8c
Kar0lLasek <19karolasek98@gmail.com>
2019-09-27 08:22:28 +0200

    ChangeThePath

--- Forms/ChangeThePath.Designer.cs
+++ Forms/ChangeThePath.Designer.cs
@@ -37,7 +37,7 @@
             // 
             this.textBox1.Location = new System.Drawing.Point(50, 59);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(525, 26);
+            this.textBox1.Size = new System.Drawing.Size(553, 26);
             this.textBox1.TabIndex = 0;
             this.textBox1.TextChanged += new System.EventHandler(this.TextBox1_TextChanged);
             // 
@@ -46,13 +46,13 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(46, 36);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(249, 20);
+            this.label1.Size = new System.Drawing.Size(568, 20);
             this.label1.TabIndex = 1;
-            this.label1.Text = "Write or paste path for templates: ";
+            this.label1.Text = "Write or paste path for templates: (e.g. C:\\Users\\ELASKAR\\Documents\\Forms) ";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(281, 119);
+            this.button1.Location = new System.Drawing.Point(291, 121);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 35);
             this.button1.TabIndex = 2;
@@ -64,10 +64,10 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(629, 204);
+            this.ClientSize = new System.Drawing.Size(662, 204);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Name = "ChangeThePath";
             this.Text = "ChangeThePath";
             this.ResumeLayout(false);
